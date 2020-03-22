24578177c4818afc8e3c8c5b1284fb85e0d7d5e1
Sayed Noorizadeh <SayedN@mobizinc.com>
2020-03-21 22:12:58 -0700

    a few more files

---
+++ say_if/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace say_if
+{
+    public class Animal
+    {
+        public string name;
+        public int legs;
+
+        public void Sound( string sound)
+        {
+            Console.WriteLine(sound);
+
+        }
+        public void move(string move)
+        {
+            Console.WriteLine("I am " + move + "ing! ");
+        }
+
+    }
+}
---
+++ say_if/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace say_if
+{
+    class Program
+    {
+        
+        static int gimmeRandome()
+        {
+            Random r  = new Random();
+            return r.Next(1,4);
+
+        }
+        static void Main(string[] args)
+        {
+            //var answer = "";
+            //Animal dog = new Animal();
+            //dog.Sound("woof woof ");
+            //dog.move("walk");
+            //Console.WriteLine("Please enter a key to continue");
+            //Console.ReadKey();
+            while (true)
+            {
+                Console.Write("Computer guessing, press space-bar when ready:");
+                var key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Spacebar)
+                    continue;
+                var strReady = gimmeRandome();
+               
+                //System.Threading.Thread.Sleep(2500); 
+                Console.WriteLine("hello to you!");
+                if (strReady == 1)
+                {
+                    Console.WriteLine("You said Rock!");
+                   
+                   
+                }
+
+                if (strReady == 2)
+                {
+                    Console.WriteLine("You said Paper!");
+
+                    
+                }
+
+                if (strReady == 3)
+                {
+                    Console.WriteLine("You said scissor!");
+
+                   
+                }
+            }
+           
+            
+        }
+    }
+}
